183c1a9ff2f2e69ed59b34283b957a821600e986
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-06 13:01:41 +0800

    Update Mainprogram.py

--- Mainprogram.py
+++ Mainprogram.py
@@ -5,3 +5,5 @@ option = st.sidebar.selectbox( '選擇球隊？', ['中信兄弟', '統一7-ELEV
 option1 = st.sidebar.selectbox( '選擇所想查看的數據？成績至2014結算至2021由於有些球隊已易主或重新加入，有些數據不可考。', ['球隊成績', '投手成績', '打擊成績', '守備成績'])
 expander = st.sidebar.expander("專用數據翻譯")
 expander.write("ERA自責分率 StrikeOut三振 BB四死球 Home主場 Away客場 BattingAvg打擊率 OBP上壘率 SLG長打率 Hit安打 Homerun全壘打 FPCT守備率 E失誤")
+if option == '中信兄弟':
+  Atlantic.Brothers()
