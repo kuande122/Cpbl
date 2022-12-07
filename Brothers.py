f9d3ffaa565c81b0a844884d3868fb10bb11bc75
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-07 11:16:35 +0800

    Update Brothers.py

--- Brothers.py
+++ Brothers.py
@@ -8,7 +8,7 @@ def Brothers():
     image = Image.open('Brothers/Brothers.png')
     st.image(image)
   with col2:
-    st.write(""##擁有者:中信金控"")
+    st.write(""擁有者:中信金控"")
     st.write('領隊:劉志威')
     st.write('總教練:林威助')
     st.write('識別色彩:黃色')
