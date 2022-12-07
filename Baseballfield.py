94640d5331d1ee84675c0a8e0e52e8719d0c1847
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-07 23:21:39 +0800

    Update Baseballfield.py

--- Baseballfield.py
+++ Baseballfield.py
@@ -1,6 +1,6 @@
 from streamlit_folium import folium_static #地圖套件
 import folium 
-def TaichungIntercontinentalBaseballStadium():
+def Taichung():
     with col1:
         st.header('主場:臺中洲際棒球場')
         st.write('地址：臺中市北屯區崇德路三段835號')
