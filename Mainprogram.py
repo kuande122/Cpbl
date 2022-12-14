e0c0f6f48b9c9c35dcb74adec2ddc9107c98eb05
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 10:01:18 +0800

    Update Mainprogram.py

--- Mainprogram.py
+++ Mainprogram.py
@@ -13,7 +13,7 @@ st.title('中華職棒資訊面板系統')
 st.sidebar.header('選擇球隊及數據')
 
 team_list={'中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', '味全龍','台鋼雄鷹'}
-question_list={'球隊成績', '投手成績', '打擊成績', '守備成績'}
+question_list={'投手成績', '打擊成績','球隊成績', '守備成績'}
 option = st.sidebar.selectbox( '選擇球隊？',team_list)
 option1 = st.sidebar.selectbox( '選擇所想查看的數據？成績至2014結算至2021由於有些球隊已易主或重新加入，有些數據不可考。', question_list)
 
