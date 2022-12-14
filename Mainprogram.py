21e58c6d94d534fa68550ac569340b8de2e2a1d2
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 12:20:32 +0800

    Update Mainprogram.py

--- Mainprogram.py
+++ Mainprogram.py
@@ -58,6 +58,6 @@ with col2:
               color='項目',
               legend='bottom',
               use_container_width=True)
- st.markdown('### 2022年年度主視覺')
- image = Image.open('年度主視覺/option'.png')
- st.image(image)
+st.markdown('### 2022年年度主視覺')
+image = Image.open('年度主視覺/option'.png')
+st.image(image)
