129addf29105bc3cacdb0d1c430a57f1e766d948
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 11:06:43 +0800

    Update Main_program.py

--- Main_program.py
+++ Main_program.py
@@ -11,6 +11,10 @@ st.title('中華職棒資訊面板系統')
 #-----Sidebar----------------------------------------------------
 image=Image.open('CPBL logo.png')
 st.sidebar.image(image)
-st.sidebar.title('請選擇區域及球隊')
+st.sidebar.title('請選擇球隊及想查看的數據')
 teams_list={'中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', '味全龍','台鋼雄鷹'}
 option_teams = st.sidebar.selectbox('選擇球隊？',teams_list)
+data_list={'投手成績', '打擊成績','球隊成績', '守備成績'}
+option_data = st.sidebar.selectbox('選擇球隊？',data_list)
+expander = st.sidebar.expander("專用數據翻譯")
+expander.write("ERA自責分率":"StrikeOut三振":"BB四死球":"Home主場":"Away客場":"BattingAvg打擊率":"OBP上壘率":"SLG長打率":"Hit安打":"Homerun全壘打":"FPCT守備率":"E失誤")
