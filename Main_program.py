b646b0dac665f0937148086d408695372ef545e6
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 12:11:09 +0800

    Update Main_program.py

--- Main_program.py
+++ Main_program.py
@@ -32,5 +32,5 @@ Baseballfield.Baseballfield(option_teams)
 
 #-----年度主視覺-----------------------------------------------------------
 st.markdown('### 2022年年度主視覺')
-image = Image.open("teams_information"+"/"+option_teams[teams_list]+"/"+option_teams+".jpg")
+image = Image.open("teams_information"+"/"+teams_list[option_teams]+"/"+option_teams+".jpg")
 st.image(image)
