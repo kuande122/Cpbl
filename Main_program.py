84aee431f8d4542141b0964c45d368c473e6ffbf
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-20 11:01:03 +0800

    Update Main_program.py

--- Main_program.py
+++ Main_program.py
@@ -1,1 +1,8 @@
+import streamlit as st  #streamlit套件
 
+
+#-----Set up-----------------------------------------------------
+st.set_page_config(page_title="CPBL Dashboard",
+                   page_icon='⚾',
+                   layout="wide")
+st.title('中華職棒資訊面板系統')
