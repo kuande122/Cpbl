855d1a8f09abd98d07cbd02df4368fb0c2e189e7
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-27 21:32:18 +0800

    Update chart.py

--- chart.py
+++ chart.py
@@ -3,7 +3,6 @@ import xlrd             #excel套件
 import openpyxl         #excel套件
 import pandas as pd     #pandas套件(資料分析)
 import matplotlib.pyplot as plt #matplotlib(資料繪圖)
-import plost  
 
 
 def chart(option_data):
