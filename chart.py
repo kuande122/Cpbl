f73fca313761666a110cd11f5a73b0baa3dc0333
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-27 21:43:51 +0800

    Update chart.py

--- chart.py
+++ chart.py
@@ -53,3 +53,17 @@ def chart(option_data):
       plt.legend(labels=["Brothers Defense", "Unilions Defense","Dragons Defense","Guardians Defense","Rakuten Defense","TSGHAWKS Defense"], loc = 'best')
       st.pyplot(plt) 
       break
+with col2:
+    if option_data=='打擊成績':
+      plt.style.use("ggplot")
+      plt.plot(data1.年度, data1.長打率,'.-' ,color='yellow') 
+      plt.plot(data2.年度, data2.長打率,'.-' ,color='darkorange')
+      plt.plot(data3.年度, data3.長打率, '.-',color='red')
+      plt.plot(data4.年度, data4.長打率,'.-', color='darkblue')
+      plt.plot(data5.年度, data5.長打率,'.-', color='maroon')
+      plt.plot(data6.年度, data6.長打率,'.-', color='darkgreen')
+      plt.xlabel('Season',fontsize="10")
+      plt.title('Batting Avg')
+      plt.legend(labels=["Brothers Batting", "Unilions Batting","Dragons Batting","Guardians Batting","Rakuten Batting","TSGHAWKS Batting"], loc = 'best')
+      st.pyplot(plt) 
+      break
